4a32e879de01664b090eefd9b6bd9ebb09d2df90
truongvinh97 <70201168+truongvinh97@users.noreply.github.com>
2025-04-29 14:11:33 +0700

    Add files via upload

--- updateService/test.js
+++ updateService/test.js
@@ -1,23 +1,22 @@
-// test-listen-event.js
 import Web3 from "web3";
 import dotenv from "dotenv";
 import fs from "fs/promises";
 dotenv.config();
+const RPC_URL = "ws://192.168.1.24:7545";
+const FIRMWARE_ADDRESS = "0xE9f1A121627Ff693f200BEFf32414fdd2E90624d";
 
-// 1. Connect WebSocket to Ganache
-const web3 = new Web3(new Web3.providers.WebsocketProvider("ws://192.168.1.24:7545"));
+// Force WebSocket connection
+const web3 = new Web3(new Web3.providers.WebsocketProvider(RPC_URL));
 
-// 2. Load ABI file
 async function loadABI() {
   const abiRaw = await fs.readFile("./src/web3/abi/FirmwareRegistry.json", "utf8");
   const abiJson = JSON.parse(abiRaw);
   return abiJson.abi;
 }
 
-// 3. Main function
 async function main() {
   const abi = await loadABI();
-  const address = "0xE9f1A121627Ff693f200BEFf32414fdd2E90624d";  // Đảm bảo .env có FIRMWARE_ADDRESS
+  const address = FIRMWARE_ADDRESS;
   const Firmware = new web3.eth.Contract(abi, address);
 
   console.log("[Test] Subscribing to NewFirmware events...");
@@ -31,9 +30,8 @@ async function main() {
       console.log(event.returnValues);
     })
     .on("error", (err) => {
-      console.error("🛑 Error:", err);
+      console.error("🛑 Error while listening:", err.message);
     });
 }
 
-// 4. Run main
 main().catch(console.error);
