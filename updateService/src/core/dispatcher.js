fca0486df7591c700de7f8c9cc2b5bb5472a7a87
truongvinh97 <70201168+truongvinh97@users.noreply.github.com>
2025-05-18 19:08:35 +0700

    Add files via upload

--- updateService/src/core/dispatcher.js
+++ updateService/src/core/dispatcher.js
@@ -25,7 +25,7 @@ import {
   aesGcmDecrypt,
   verifySig,
   sha256
-} from "../services/crypto.js";
+} from "../lib/crypto.js";
 
 // load DeviceConfiguration.json
 const require = createRequire(import.meta.url);
